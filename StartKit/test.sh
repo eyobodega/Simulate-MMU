8177ab51452f7b847bfa231b892b04e1186c4561
hamzehkhazaei <hamzeh.khazaei@ieee.org>
2021-03-19 01:14:42 -0400

    changing the test file

--- StartKit/test.sh
+++ StartKit/test.sh
@@ -11,5 +11,5 @@ diff output256.csv correct256.csv
 echo "--------------- Phase 2 ---------------------"
 echo "Running Phase 2"
 ./mmu 128 BACKING_STORE.bin addresses.txt
-# echo "Comparing Phase 2"
+echo "Comparing Phase 2"
 # diff output128.csv correct128.csv
